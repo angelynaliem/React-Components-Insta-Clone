c312877513d8c8d599f3f18ec18fc85034749d6b
angelynaliem <angelynaliem@gmail.com>
2020-07-22 21:06:22 -0400

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const [posts, setPosts] = useState({dummyData});
+  const [posts, setPosts] = useState(dummyData);
 
   const likePost = postId => {
 
@@ -38,21 +38,20 @@ const App = () => {
      setPosts(
        posts.map(post => {
          if(post.id === postId) {
-           return {...post};
+           return {...post, likes: post.likes + 1};
        } else {
          return post;
        }
       })
      )
-
   };
 
   return (
     <div className='App'>
       {/* // Add SearchBar and Posts here to render them */}      
       {/* // Check the implementation of each component, to see what props they require, if any! */}
-      <Posts posts={posts} likePost={likePost}/>
       <SearchBar/>
+      <Posts posts={posts} likePost={likePost}/>
     </div>
   );
 };
